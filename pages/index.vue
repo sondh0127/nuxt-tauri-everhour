b5d9eb1c4431a85c12f461bac5d017d3d059c071
Son Hong Do <sondh0127@gmail.com>
2024-02-20 15:08:35 +0700

    remove

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import { Command } from '@tauri-apps/api/shell'
+// import { Command } from '@tauri-apps/api/shell'
 
 async function run() {
-  const command = Command.sidecar('binaries/example')
-  const output = await command.execute()
+  // const command = Command.sidecar('binaries/example')
+  // const output = await command.execute()
 }
 </script>
 
